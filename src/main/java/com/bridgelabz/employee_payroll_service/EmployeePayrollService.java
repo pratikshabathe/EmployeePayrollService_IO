920022181aa4df31c85f2a76644c1b05108f7783
pratikshabathe <pratikshabathe11@gmail.com>
2023-02-27 15:50:45 +0530

    Added: to print the EmpPayroll & show number of entries

--- src/main/java/com/bridgelabz/employee_payroll_service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employee_payroll_service/EmployeePayrollService.java
@@ -10,66 +10,72 @@ import java.util.Scanner;
 
 public class EmployeePayrollService {
 	
-	 private List<EmployeePayrollData> employePayrollList = new ArrayList<EmployeePayrollData>();
-	    Scanner scanner = new Scanner(System.in);
-	    private static final String FILE_PATH = "C:\\Users\\MY PC\\Desktop\\PayRoll.txt";
+	private List<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+    Scanner scanner = new Scanner(System.in);
+    private static final String FILE_PATH = "C:\\Users\\User\\Desktop";
 
-	    public void readEmployeeDataFromConsole() {
-	        System.out.println("Enter Employee Id");
-	        int id = scanner.nextInt();
-	        System.out.println("Enter Employee Name");
-	        String Name = scanner.next();
-	        System.out.println("Enter the salary");
-	        int salary = scanner.nextInt();
-	        employePayrollList.add(new EmployeePayrollData(id, Name, salary));
-	    }
-
-	    public void writeEmployeeDataInConsole() {
-	        System.out.println("Writing Employee Pay Roll Data \n" + employePayrollList);
-	    }
-
-	    public void addEmployee(EmployeePayrollData employee) {
-	        employePayrollList.add(employee);
-	    }
-
-	    public void writeEmployeeDataToFile() {
-	        checkFile();
-	        StringBuffer empBuffer = new StringBuffer();
-	        employePayrollList.forEach(employee -> {
-	            String employeeDataString = employee.toString().concat("\n");
-	            empBuffer.append(employeeDataString);
-	        });
-	        try {
-	            Files.write(Paths.get(FILE_PATH), empBuffer.toString().getBytes());
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-
-	    // method to create file if file doesn't exist
-	    private void checkFile() {
-	        File file = new File(FILE_PATH);
-	        try {
-	            // checking file already exists
-	            if (!file.exists()) {
-	                // if not creating a new file
-	                file.createNewFile();
-	                System.out.println("Created a file at " + FILE_PATH);
-	            }
-	        } catch (IOException e1) {
-	            System.err.println("Problem encountered while creating a file");
-	        }
-	    }
-
-	    public long countEntries() {
-	        long entries = 0;
-	        try {
-	            entries = Files.lines(new File(FILE_PATH).toPath()).count();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	        return entries;
-	    }
+    public void readEmployeeDataFromConsole() {
+        System.out.println("Enter Employee Id");
+        int id = scanner.nextInt();
+        System.out.println("Enter Employee Name");
+        String Name = scanner.next();
+        System.out.println("Enter the salary");
+        int salary = scanner.nextInt();
+        employeePayrollList.add(new EmployeePayrollData(id, Name, salary));
+    }
+    public void writeEmployeeDataInConsole() {
+        System.out.println("Writing Employee Pay Roll Data \n" + employeePayrollList);
+    }
+    public void addEmployee(EmployeePayrollData employee) {
+        employeePayrollList.add(employee);
+    }
+    public void writeEmployeeDataToFile() {
+        checkFile();
+        StringBuffer empBuffer = new StringBuffer();
+        employeePayrollList.forEach(employee -> {
+            String employeeDataString = employee.toString().concat("\n");
+            empBuffer.append(employeeDataString);
+        });
+        try {
+            Files.write(Paths.get(FILE_PATH), empBuffer.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void checkFile() {
+        File file = new File(FILE_PATH);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+                System.out.println("Created a file at " + FILE_PATH);
+            }
+        } catch (IOException e1) {
+            System.err.println("Problem encountered while creating a file");
+        }
+    }
+    public long countEntries() {
+        long entries = 0;
+        try {
+            entries = Files.lines(new File(FILE_PATH).toPath()).count();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return entries;
+    }
+    public void printData() {
+        try {
+            Files.lines(Paths.get(FILE_PATH)).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        System.out.println("Welcome to employee payRoll");
+        employeePayrollService.readEmployeeDataFromConsole();
+        employeePayrollService.writeEmployeeDataInConsole();
+    }
 }
 
  
--- src/test/java/com/bridgelabz/employee_payroll_service/EmployeePayrollTest.java
+++ src/test/java/com/bridgelabz/employee_payroll_service/EmployeePayrollTest.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employee_payroll_service;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class EmployeePayrollTest {
@@ -14,6 +15,29 @@ public class EmployeePayrollTest {
 	        employeePayrollService.addEmployee(new EmployeePayrollData(3, "Charlie ", 30000));
 	        employeePayrollService.writeEmployeeDataToFile();
 	        long entries = employeePayrollService.countEntries();
-	        assertEquals(3, entries);
+	        Assert.assertEquals(3, entries);
+	    }
+	 @Test
+	    public void given3EmployeesWhenWrittenToFileShouldPrintEmployeeEntries() {
+	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+	        employeePayrollService.addEmployee(new EmployeePayrollData(1, "Bill", 10000));
+	        employeePayrollService.addEmployee(new EmployeePayrollData(2, "Tersia", 20000));
+	        employeePayrollService.addEmployee(new EmployeePayrollData(3, "Charlie", 30000));
+	        employeePayrollService.writeEmployeeDataToFile();
+	        employeePayrollService.printData();
+	        long entries = employeePayrollService.countEntries();
+	        Assert.assertEquals(3, entries);
+	    }
+
+	    @Test
+	    public void given3EmployeesWhenWrittenToFileShouldPrintNumberOfEmployeeEntries() {
+	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+	        employeePayrollService.addEmployee(new EmployeePayrollData(1, "Bill", 10000));
+	        employeePayrollService.addEmployee(new EmployeePayrollData(2, "Tersia", 20000));
+	        employeePayrollService.addEmployee(new EmployeePayrollData(3, "Charlie", 30000));
+	        employeePayrollService.writeEmployeeDataToFile();
+	        long entries = employeePayrollService.countEntries();
+	        System.out.println("Number of entries are " + entries);
+	        Assert.assertEquals(3, entries);
 	    }
 }
